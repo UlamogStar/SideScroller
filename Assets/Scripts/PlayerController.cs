12702259e9d96cf69031af5a7004d442606b3b55
ElijahL <vigorilious@gmail.com>
2023-09-01 14:32:26 -0600

    Attempts at polishing, SFX, and the addition of end game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,46 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
 {  
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
     public float gravity = -20f;
+    public int maxJumps =1;
+
     public Transform groundCheck;
     public LayerMask groundLayer;
+    
     private Rigidbody rb;
     private Vector3 velocity;
-    
+   
     private CharacterController controller;
     private bool isGrounded;
-
+    private float groundCheckRadius = 2f;
+    private int jumpsRemaining;
+    private AudioSource audioSource;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        jumpsRemaining = maxJumps;
+        audioSource = GetComponent<AudioSource>();
     }
     private void Update()
     {
         // Check if the player is grounded
+         bool isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.1f);
          isGrounded = Physics.CheckSphere(groundCheck.position, 0.1f, groundLayer);
+        
+
 
 
         // Player movement
         float horizontalInput = Input.GetAxis("Horizontal");
+       
         Vector3 moveDirection = new Vector3(horizontalInput, 0f, 0f);
         Vector3 move = transform.TransformDirection(moveDirection) * moveSpeed;
         rb.velocity = new Vector3(moveDirection.x * moveSpeed, rb.velocity.y, 0f);
-        
+       
+
 
         // Jumping
-        if (isGrounded && Input.GetButtonDown("Jump"))
+        if (isGrounded)
         {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            velocity.y = Mathf.Sqrt(jumpForce * -2 * gravity);
+            jumpsRemaining = maxJumps;  // Reset jumps when grounded.
         }
-    
+
+        if (jumpsRemaining > 0 && Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 0f, 0f);  // Zero out vertical velocity.
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jumpsRemaining--;
+
+            audioSource.Play();
+        }
+   
     }
-    public void GiveHealth(int amount)
-    {
-        Debug.Log("Player picked up health");
-    }
+
+
 }
+
