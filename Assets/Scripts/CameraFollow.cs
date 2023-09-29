b5457aff1d83d3b0178ac8438454f5fb226ebcd0
ElijahL <vigorilious@gmail.com>
2023-09-29 14:20:26 -0600

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,21 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
-    public Transform target;        // The target to follow (usually the player's transform)
-    public Vector3 offset = new Vector3(0f, 0f, -10f); // Offset from the target in world space
-    public float smoothing = 5f;    // The speed at which the camera follows the target
+<<<<<<< Updated upstream
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+=======
+<<<<<<< Updated upstream
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+=======
+    public Transform target;    
+    public Vector3 offset = new Vector3(0f, 0f, -10f); 
+    public float smoothing = 5f;    
 
     private void FixedUpdate()
     {
-        // Calculate the desired camera position
         Vector3 desiredPosition = target.position + offset;
 
-        // Smoothly interpolate between the current camera position and the desired position
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothing * Time.deltaTime);
 
-        // Set the camera's position to the smoothed position
         transform.position = smoothedPosition;
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,49 +1,65 @@
+<<<<<<< Updated upstream
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+=======
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+<<<<<<< Updated upstream
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+=======
 public class PlayerController : MonoBehaviour
-{  
-    public float moveSpeed = 10f;
-    public float jumpForce = 5f;
-    public float gravity = -20f;
-    public int maxJumps =2;
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public int maxJumps = 2;  // Set the maximum number of jumps.
 
-    public Transform groundCheck;
-    public LayerMask groundLayer;
-    
     private Rigidbody rb;
-    private Vector3 velocity;
-   
-    private CharacterController controller;
-    private bool isGrounded;
     private int jumpsRemaining;
-    private AudioSource audioSource;
-    private void Start()
+
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
         jumpsRemaining = maxJumps;
-        audioSource = GetComponent<AudioSource>();
     }
-    private void Update()
+
+    void Update()
     {
         // Check if the player is grounded
-         bool isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.1f);
-            
-        //isGrounded = Physics.CheckSphere(groundCheck.position, 0.1f, groundLayer);
-        
-
-
+        bool isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.1f);
 
         // Player movement
         float horizontalInput = Input.GetAxis("Horizontal");
-       
         Vector3 moveDirection = new Vector3(horizontalInput, 0f, 0f);
-        Vector3 move = transform.TransformDirection(moveDirection) * moveSpeed;
         rb.velocity = new Vector3(moveDirection.x * moveSpeed, rb.velocity.y, 0f);
-       
-
 
         // Jumping
         if (isGrounded)
@@ -53,15 +69,13 @@ public class PlayerController : MonoBehaviour
 
         if (jumpsRemaining > 0 && Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector3(rb.velocity.x, 0f, 0f);  // Zero out vertical velocity.
+            Debug.Log("Jumping!");
+            rb.velocity = new Vector3(rb.velocity.x, 0f, 0f);
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             jumpsRemaining--;
-
-            audioSource.Play();
         }
-   
+
+>>>>>>> Stashed changes
     }
-
-
 }
-
+>>>>>>> Stashed changes
